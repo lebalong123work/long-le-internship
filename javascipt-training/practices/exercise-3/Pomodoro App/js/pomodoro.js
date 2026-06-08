c3f14a945c002d3d6b6f0968f04a1caaef5c17aa
longle <lebalong1802@gmail.com>
2026-06-08 17:02:31 +0700

    Exercise 3: addTask

--- javascipt-training/practices/exercise-3/Pomodoro App/js/pomodoro.js
+++ javascipt-training/practices/exercise-3/Pomodoro App/js/pomodoro.js
@@ -1,3 +1,18 @@
 let tasks = [];
 const pomodoroForm = document.querySelector(".js-add-task");
 const pomodoroTableBody = document.querySelector(".js-task-table-body");
+
+const addTask = function (event) {
+  event.preventDefault();
+  const taskName = this.querySelector(".js-task-name").value;
+  const pomodoroCount = this.querySelector(".js-pomodoro-count").value;
+  this.reset();
+  tasks.push({
+    taskName,
+    pomodoroDone: 0,
+    pomodoroCount,
+    finished: false,
+  });
+  renderTasks(pomodoroTableBody, tasks);
+};
+pomodoroForm.addEventListener("submit", addTask);

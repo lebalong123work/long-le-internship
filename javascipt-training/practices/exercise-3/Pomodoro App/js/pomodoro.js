79d321c55a0e316fcf51e731d100b19fe12697d5
longle <lebalong1802@gmail.com>
2026-06-09 10:28:18 +0700

    Exercise 3: renderTasks

--- javascipt-training/practices/exercise-3/Pomodoro App/js/pomodoro.js
+++ javascipt-training/practices/exercise-3/Pomodoro App/js/pomodoro.js
@@ -16,3 +16,26 @@ const addTask = function (event) {
   renderTasks(pomodoroTableBody, tasks);
 };
 pomodoroForm.addEventListener("submit", addTask);
+
+const renderTasks = function (tBodyNode, tasks = []) {
+  tBodyNode.innerHTML = tasks
+    .map(
+      (task, id) => `
+        <tr>
+            <td class="cell-task-name">${task.taskName}</td>
+            <td class="cell-pom-count">${task.pomodoroDone} / ${task.pomodoroCount} pomodori</td>
+            <td class="cell-pom-controls">
+            ${
+              task.finished
+                ? "Finished"
+                : `
+                <button class="js-task-done" data-id="${id}">Done</button>
+                <button class="js-increase-pomodoro" data-id="${id}">Increase Pomodoro Count</button>`
+            }
+                <button class="js-delete-task" data-id="${id}">Delete Task</button>
+            </td>
+        </tr>
+    `,
+    )
+    .join("");
+};

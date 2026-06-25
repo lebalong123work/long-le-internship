8736953cb4dc5ba3642add9cb99593cc79f32134
longle <lebalong1802@gmail.com>
2026-06-25 16:39:31 +0700

    Exercise 4 (Primitive Values): string_type.js

---
+++ javascipt-training/practices/exercise-4/primitive values/string_type.js
@@ -0,0 +1,33 @@
+// Count INDEX and LENGTH
+let mess = "Hello";
+
+console.log("The character at position 0: ", mess [0]);
+console.log("The character at position 4: ", mess [4]);
+
+// leght
+console.log("Leght: ", mess.length);
+// emoji
+let icon = "🧸";
+console.log("See icon: ", icon);
+console.log("Leght icon: ", icon.length);
+
+// String IMMUTABLE
+let name = "Long"
+
+name[0] = "B";
+
+console.log("Name when change: ", name);
+
+// Want change: Make new String 
+let oldname = "Khang";
+
+// method to cut string --- substring()
+
+let newname = oldname.substring(0,3);
+
+console.log("Cut: ",newname);
+console.log("See: ",oldname);
+
+// Use + new string
+let finalname = newname + "nh";
+console.log("Final name: ", finalname);

3bb9e1e2622f3f3b4447d1f91e9b525311b6948f
longle <lebalong1802@gmail.com>
2026-07-08 10:42:54 +0700

    feat: stage 1 - Add Task Feature - toggle task form visibility when clicking Add and Cancel

--- javascipt-training/practices-pomofocus/index.js
+++ javascipt-training/practices-pomofocus/index.js
@@ -1,4 +1,24 @@
-console.log("connect!");
+const showTaskFormBtn = document.getElementById("showTaskFormBtn");
+const taskFormContainer = document.getElementById("taskFormContainer");
+const cancelTaskBtn = document.getElementById("cancelTaskBtn");
+const saveTaskBtn = document.getElementById("saveTaskBtn");
+const taskNameInput = document.getElementById("taskNameInput");
+const estPomodorosInput = document.getElementById("estPomodorosInput");
+const taskList = document.getElementById("taskList");
 
 let tasks = [];
-console.log("Current task data:", tasks);
+
+taskFormContainer.style.display = "none";
+
+showTaskFormBtn.addEventListener("click", () => {
+  showTaskFormBtn.style.display = "none";
+  taskFormContainer.style.display = "block";
+});
+
+cancelTaskBtn.addEventListener("click", () => {
+  taskFormContainer.style.display = "none";
+  showTaskFormBtn.style.display = "block";
+
+  taskNameInput.value = "";
+  estPomodorosInput.value = "1";
+});

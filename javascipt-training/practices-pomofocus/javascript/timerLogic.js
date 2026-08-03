7bdd69ec9c0648f53916c16a5d03e2075df59d87
longle <lebalong1802@gmail.com>
2026-08-03 17:03:13 +0700

    feat: stage 3 - Setup Timer

---
+++ javascipt-training/practices-pomofocus/javascript/timerLogic.js
@@ -0,0 +1,73 @@
+export function formatTime(totalSeconds) {
+  const minutes = Math.floor(totalSeconds / 60);
+
+  const seconds = totalSeconds % 60;
+
+  const stringMinutes = minutes.toString().padStart(2, "0");
+
+  const stringSeconds = seconds.toString().padStart(2, "0");
+
+  return `${stringMinutes}:${stringSeconds}`;
+}
+
+let currentDuration = 25 * 60;
+let timeLeft = currentDuration;
+let isRunning = false;
+let timerId = null;
+
+let onTickCallback = null;
+let onCompleteCallback = null;
+
+export function setTimerCallback(callback) {
+  onTickCallback = callback;
+}
+
+export function setTimerCompleteCallback(callback) {
+  onCompleteCallback = callback;
+}
+
+export function toggleTimer() {
+  if (isRunning) {
+    clearInterval(timerId);
+    isRunning = false;
+  } else {
+    isRunning = true;
+
+    timerId = setInterval(() => {
+      timeLeft--;
+
+      if (onTickCallback) {
+        onTickCallback(formatTime(timeLeft));
+      }
+
+      if (timeLeft <= 0) {
+        clearInterval(timerId);
+        isRunning = false;
+
+        if (onCompleteCallback) {
+          onCompleteCallback();
+        }
+
+        timeLeft = currentDuration;
+
+        if (onTickCallback) {
+          onTickCallback(formatTime(timeLeft));
+        }
+      }
+    }, 1000);
+  }
+
+  return isRunning;
+}
+
+export function setMode(minutes) {
+  clearInterval(timerId);
+  isRunning = false;
+
+  currentDuration = minutes * 60;
+  timeLeft = currentDuration;
+
+  if (onTickCallback) {
+    onTickCallback(formatTime(timeLeft));
+  }
+}

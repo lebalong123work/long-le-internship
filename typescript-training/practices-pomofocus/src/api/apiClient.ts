56efbdcbe83b86a89817c93f67fd996b602cb027
longle <lebalong1802@gmail.com>
2026-09-25 16:28:53 +0700

    fix: fix typle TS in file

--- typescript-training/practices-pomofocus/src/api/apiClient.ts
+++ typescript-training/practices-pomofocus/src/api/apiClient.ts
@@ -1,24 +1,31 @@
 import { CONFIG } from "../config/config.js";
+export interface ApiRequestOptions extends RequestInit {
+  headers?: Record<string, string>;
+}
 
 export async function fetchAPI<T>(
   endpoint: string,
-  options: RequestInit = {},
-): Promise<T> {
+  options: ApiRequestOptions = {},
+): Promise<T | boolean> {
   const url: string = `${CONFIG.API.BASE_URL}${endpoint}`;
   const token: string | null = localStorage.getItem("accessToken");
 
-  const headers = new Headers(options.headers);
-
-  if (token !== null) {
-    headers.set("Authorization", `Bearer ${token}`);
+  if (options.headers === undefined) {
+    options.headers = {};
   }
 
-  options.headers = headers;
+  if (token !== null) {
+    options.headers["Authorization"] = `Bearer ${token}`;
+  }
 
   const response: Response = await fetch(url, options);
 
   if (!response.ok) {
     throw new Error(`API Request Failed: ${response.status}`);
+  }
+
+  if (options.method === "DELETE") {
+    return response.ok;
   }
 
   return (await response.json()) as T;
